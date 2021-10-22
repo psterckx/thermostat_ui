3b636bd030ebff34dd5aab116f7e79769a085d7f
Peter Sterckx <pasterckx@gmail.com>
2021-10-21 20:52:04 -0400

    Update readme, clean up

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
-// import Home from './Home.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 Vue.use(VueRouter);
 
 Vue.config.productionTip = false
-
 
 const router = new VueRouter({
   mode: 'history'
